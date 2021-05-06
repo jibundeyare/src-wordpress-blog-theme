729e72e205157161d34df606c4b02775d76e7e0b
jibundeyare <contact@jibundeyare.com>
2021-05-06 18:07:09 +0200

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+get_header();
+
+?>
+<div class="container">
+    <article>
+        <div class="row">
+            <div class="col-sm">
+                <h1>404 Not found</h1>
+                <div>Désolé :(</div>
+            </div>
+        </div>
+    </article>
+</div>
+<?php
+
+get_footer();
+
