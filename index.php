aec86f375d1a7aaf6f1c48f4db639b9731c644d7
jibundeyare <contact@jibundeyare.com>
2022-06-15 15:54:32 +0200

    Improve code

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if ( have_posts() ):
     <article>
         <div class="row">
             <div class="col-lg-10 offset-lg-2">
-                <h1><a href="<?= get_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <h2><a href="<?= get_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <div><em><?php the_time( get_option( 'date_format' ) ); ?></em></div>
             </div>
         </div>
